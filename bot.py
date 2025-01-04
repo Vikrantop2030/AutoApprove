61da08c3ce19ec3a7eae1efcf7a3681a4e63c44a
Vikrantop2030 <bgmiidsdata@gmail.com>
2025-01-04 07:59:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,7 @@
 import asyncio
 import random
 from pyrogram import Client, filters
-from pyrogram.types import (ChatJoinRequest, InlineKeyboardButton,
-                            InlineKeyboardMarkup, Message)
+from pyrogram.types import ChatJoinRequest, Message
 from pyrogram.errors import ChatAdminRequired, FloodWait, UserIsBlocked, PeerIdInvalid
 
 import config
@@ -28,29 +27,20 @@ async def create_approve_task(app: Client, j: ChatJoinRequest, after_delay: int)
         if chat.username:
             # Public channel link
             channel_link = f"https://t.me/{chat.username}"
-            # Inline button for public channel
-            buttons = [
-                [InlineKeyboardButton("View Channel", url=channel_link)]
-            ]
-            welcome_text = f"Hey {user.first_name}!\nWelcome to {chat.title}\n\nYour request to join {chat.title} has been accepted.\n\nClick below to view the channel:"
+            welcome_text = f"Hey {user.first_name}!\nWelcome to {chat.title}\n\nYour request to join {chat.title} has been accepted.\n\nYou can view the channel here: {channel_link}"
         else:
             # Private channel: Generate an invite link if the bot has permission
             try:
                 link = await app.export_chat_invite_link(chat.id)
                 channel_link = link
-                # Display channel preview for private channel
-                buttons = [
-                    [InlineKeyboardButton("View Channel", url=channel_link)]
-                ]
-                welcome_text = f"Hey {user.first_name}!\nWelcome to {chat.title}\n\nYour request to join {chat.title} has been accepted.\n\nClick below to view the private channel:"
+                welcome_text = f"Hey {user.first_name}!\nWelcome to {chat.title}\n\nYour request to join {chat.title} has been accepted.\n\nYou can view the private channel here: {channel_link}"
             except ChatAdminRequired:
                 # If the bot can't generate the invite link
                 channel_link = "Private channel. Contact admin for an invite link."
-                buttons = []
                 welcome_text = f"Hey {user.first_name}!\nWelcome to {chat.title}\n\nYour request to join {chat.title} has been accepted.\n\n{channel_link}"
 
-        # Send the welcome message with a GIF and inline button
-        await app.send_animation(chat_id=user.id, animation=gif, caption=welcome_text, reply_markup=InlineKeyboardMarkup(buttons))
+        # Send the welcome message with a GIF
+        await app.send_animation(chat_id=user.id, animation=gif, caption=welcome_text)
     except (UserIsBlocked, PeerIdInvalid):
         pass
 
@@ -76,13 +66,7 @@ async def approval(app: Client, m: ChatJoinRequest):
 async def start(app: Client, msg: Message):
     await msg.reply_photo(
         photo="https://telegra.ph/file/f394c45e5f2f147a37090.jpg",
-        caption=f"Hello {msg.from_user.mention},\n\nI am an auto-approve bot that helps admins manage join requests.\nAdd me to your group and make me an admin to start using the bot.",
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [InlineKeyboardButton(f"Add {app.me.first_name}", url=f"https://t.me/{app.me.username}?startgroup=true")],
-                [InlineKeyboardButton("Channel", url="https://yourchannel.link/")]
-            ]
-        )
+        caption=f"Hello {msg.from_user.mention},\n\nI am an auto-approve bot that helps admins manage join requests.\nAdd me to your group and make me an admin to start using the bot."
     )
 
 # Handle private group start
@@ -91,7 +75,8 @@ async def gc(app: Client, msg: Message):
     add_group(msg.chat.id)
     if msg.from_user:
         add_user(msg.from_user.id)
-    await msg.reply_text(text=f"{msg.from_user.mention}, Start me in private for more info..", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Start Me In Private", url=f"https://t.me/{app.me.username}?start=start")]]))
+    await msg.reply_text(text=f"{msg.from_user.mention}, Start me in private for more info..")
+
 
 # Broadcast functionality and other commands (not included for brevity)
 
